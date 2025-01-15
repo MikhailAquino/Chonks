135182a59216c3bc9215543c404a789b52bca3ee
jiji-3030 <157555761+jiji-3030@users.noreply.github.com>
2025-01-15 16:51:57 +0800

    initial commit

---
+++ src/gui/src/main/java/com/mycompany/ppl_lexical/PPL_Lexical.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ppl_lexical;
+
+/**
+ *
+ * @author Jae
+ */
+public class PPL_Lexical {
+
+    public static void main(String[] args) {
+        GUI Lexical = new GUI();
+        Lexical.setVisible(true);
+    }
+
+
+}
